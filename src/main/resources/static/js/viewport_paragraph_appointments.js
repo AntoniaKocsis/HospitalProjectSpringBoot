8eee0fd030a57dd9ebebc31e9c03ea3240006b21
Antonia2636 <antoniakocsis66@gmail.com>
2024-04-11 13:15:16 +0300

    Some new Modifications

---
+++ src/main/resources/static/js/viewport_paragraph_appointments.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function () {
+    var element = document.getElementById('appointments-paragraph');
+    var position = element.getBoundingClientRect();
+
+    // If the top of the paragraph is in the viewport and it's not visible yet, reveal it with a smooth transition
+    if (position.top <= window.innerHeight && position.bottom >= 0 && getComputedStyle(element).opacity === "0") {
+        element.style.opacity = '1';
+    } else if (position.bottom < 0 || position.top > window.innerHeight) {
+        // If the paragraph is out of the viewport, reset its opacity to hide it
+        element.style.opacity = '0';
+    }
+});
